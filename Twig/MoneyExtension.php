01a47c980db617296a6dd609173831eeacdebc44
Xavier Coureau <xavier@pandawan-technology.com>
2022-06-28 11:19:57 +0200

    Adds calculation twig extensions

--- Twig/MoneyExtension.php
+++ Twig/MoneyExtension.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 namespace PandawanTechnology\MoneyBundle\Twig;
 
+use PandawanTechnology\Money\Calculator\CalculatorInterface;
 use PandawanTechnology\Money\Formatter\FormatterInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
+use Twig\TwigFunction;
 
 class MoneyExtension extends AbstractExtension
 {
-    public function __construct(private FormatterInterface $formatter)
+    public function __construct(private FormatterInterface $formatter, private CalculatorInterface $calculator)
     {
     }
 
@@ -22,6 +24,17 @@ class MoneyExtension extends AbstractExtension
         return [
             new TwigFilter('money_format', [$this->formatter, 'formatPrice']),
             new TwigFilter('currency_symbol', [$this->formatter, 'formatCurrencySymbol']),
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('money_divide', [$this->calculator, 'divide']),
+            new TwigFunction('money_multiply', [$this->calculator, 'multiply']),
         ];
     }
 }
