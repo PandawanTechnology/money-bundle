f19e3532c7c296fa6b011b4a5596766ce3493bd7
Xavier Coureau <xavier@pandawan-technology.com>
2022-07-19 08:52:42 +0200

    Improves forms

--- Form/Type/AmountFormType.php
+++ Form/Type/AmountFormType.php
@@ -4,8 +4,65 @@ declare(strict_types=1);
 
 namespace PandawanTechnology\MoneyBundle\Form\Type;
 
+use PandawanTechnology\Money\Manager\CurrencyManager;
+use PandawanTechnology\MoneyBundle\Formatter\CurrencyFormatter;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AmountFormType extends AbstractType
 {
+    public function __construct(private string $defaultCurrencyCode, private CurrencyManager $currencyManager)
+    {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getParent(): string
+    {
+        return NumberType::class;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver
+            ->setDefaults([
+                'html5' => true,
+                'currency' => null,
+                'scale' => null, // will be overridden in normalizer based on selected currency
+            ])
+            // Only enable allowed currencies. Will be set to default currency if null
+            ->setAllowedTypes('currency', ['null', 'string'])
+            ->setAllowedValues('currency', array_merge([null], $this->currencyManager->getAllowedCurrencyCodes()))
+            ->setNormalizer('currency', function (OptionsResolver $optionsResolver, ?string $currency = null) {
+                return \is_null($currency) ? $this->defaultCurrencyCode : $currency;
+            })
+
+            // Make sure that if a scale has been provided, it does not exceed the maximum length
+            ->setAllowedTypes('scale', ['int', 'null'])
+            ->setAllowedValues('scale', [null] + range(0, CurrencyFormatter::DEFAULT_DECIMAL_SCALE))
+            ->setNormalizer('scale', function (OptionsResolver $optionsResolver, ?int $scale = null) {
+                if (!\is_null($scale)) {
+                    return $scale;
+                }
+
+                return $this->currencyManager->getCurrencyPrecision($optionsResolver['currency']);
+            })
+            ->setNormalizer('attr', function (Options $options, array $currentValue) {
+                if (isset($currentValue['step'])) {
+                    return $currentValue;
+                }
+
+
+                $currentValue['step'] = 1 / pow(10, $options['scale']);
+
+                return $currentValue;
+            })
+        ;
+    }
 }
--- Form/Type/SimpleMoneyFormType.php
+++ Form/Type/SimpleMoneyFormType.php
@@ -4,18 +4,22 @@ declare(strict_types=1);
 
 namespace PandawanTechnology\MoneyBundle\Form\Type;
 
+use PandawanTechnology\Money\Factory\MoneyFactory;
 use PandawanTechnology\Money\Manager\CurrencyManager;
 use PandawanTechnology\Money\Model\Money;
-use PandawanTechnology\MoneyBundle\Formatter\CurrencyFormatter;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Event\PreSetDataEvent;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SimpleMoneyFormType extends AbstractType
 {
-    public function __construct(private string $defaultCurrencyCode, private CurrencyManager $currencyManager)
-    {
+    public function __construct(
+        private CurrencyManager $currencyManager,
+        private string $defaultCurrencyCode,
+        private MoneyFactory $moneyFactory,
+    ) {
     }
 
     /**
@@ -26,27 +30,15 @@ class SimpleMoneyFormType extends AbstractType
         $resolver
             ->setDefaults([
                 'data_class' => Money::class,
-                'html5' => true,
                 'currency' => null,
                 'amount_options' => [],
-                'scale' => null, // will be overridden in normalizer based on selected currency
             ])
-            // Only enable allowed currencies. Will be set to default currency if null
+            ->setAllowedTypes('currency', ['null', 'string'])
             ->setAllowedValues('currency', array_merge([null], $this->currencyManager->getAllowedCurrencyCodes()))
             ->setNormalizer('currency', function (OptionsResolver $optionsResolver, ?string $currency = null) {
                 return \is_null($currency) ? $this->defaultCurrencyCode : $currency;
             })
-
-            // Make sure that if a scale has been provided, it does not exceed the maximum length
-            ->setAllowedTypes('scale', ['int', 'null'])
-            ->setAllowedValues('scale', [null] + range(0, CurrencyFormatter::DEFAULT_DECIMAL_SCALE))
-            ->setNormalizer('scale', function (OptionsResolver $optionsResolver, ?int $scale = null) {
-                if (!\is_null($scale)) {
-                    return $scale;
-                }
-
-                return $this->currencyManager->getCurrencyPrecision($optionsResolver['currency']);
-            })
+            ->setAllowedTypes('amount_options', ['array'])
         ;
     }
 
@@ -56,12 +48,19 @@ class SimpleMoneyFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('amount', NumberType::class, $options['amount_options'] + [
-                'html5' => $options['html5'],
-                'attr' => [
-                    'step' => 1 / pow(10, $options['scale']),
-                ],
-            ])
+            ->add('amount', AmountFormType::class, $options['amount_options'])
+            ->addEventListener(FormEvents::PRE_SET_DATA, function (PreSetDataEvent $preSetDataEvent) {
+                $formConfig = $preSetDataEvent->getForm()->getConfig();
+
+                if (
+                    $preSetDataEvent->getData() ||
+                    !$formConfig->getOption('required')
+                ) {
+                    return;
+                }
+
+                $preSetDataEvent->setData($this->moneyFactory->createMoney(0, $formConfig->getOption('currency')));
+            })
         ;
     }
 }
